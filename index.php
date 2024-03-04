52888352dcdf3b365217e35366636aeda6dc949c
emmanuel-hartman <76754839+emmanuel-hartman@users.noreply.github.com>
2024-03-04 09:23:13 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+
+<head>
+<meta http-equiv="content-type" content="text/html;charset=utf-8"/>
+<meta http-equiv="Content-Style-Type" content="text/css"/>
+
+<title></title>
+<link href="styles/trs-style-iWeblike.css" rel="stylesheet" type="text/css" />
+<link rel="icon" href="images-static/titan.jpg" type="image/jpg" />
+</head>
+
+
+<body>
+
+<div id="navigation">
+
+<table>
+<tr>
+<td><a href="index.php">Home</a></td>
+<td><a href="research.php">Research</a></td>
+<td><a href="teaching.php">Teaching</a></td>
+<td><a href="CV.php">CV</a></td>
+<td><a href="other.php">Other</a></td>
+</tr>
+</table>
+
+
+<div class="hr"> <hr /> </div>
+
+
+
+
+</div>   <! End of navigation div -->
+
+<div id="name">
+Emmanuel Hartman
+</div>
+
+<div id="main">
+
+
+
+
+
+
+<table><tr><td>
+
+<img src="images-static/meandtitan.jpg" width="400" >
+
+</td><td>
+
+
+<div id="right">
+
+<table>
+<tr> <td>PhD Candidate in Pure Mathematics</td></tr>
+</table>
+
+<table>
+<tr> <td>Florida State University</td></tr>
+<tr><td>Department of Mathematics</td></tr>
+<tr> <td>208 Love Building, 1017 Academic Way,</td></tr>
+<tr><td> Tallahassee, Fl 32306-4510</td></tr>
+
+</table>
+
+<table>
+<tr><td><bold>Email:</bold> elh18e[at]fsu.edu</td> </tr>
+<tr><td><bold>Office:</bold> 201A Love Building </td> </tr>
+<tr><td><bold>Office Hours:</bold> By appointment </td> </tr>
+</tr>
+</table>
+
+</div>
+</td>
+</tr>
+</table>
+
+
+<div id="footer">
+
+<br clear="all" />
+
+
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
+
+
