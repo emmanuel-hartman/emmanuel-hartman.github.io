a9f9f7bb56c06045e28ed800914edf5e9bb3ca87
emmanuel-hartman <76754839+emmanuel-hartman@users.noreply.github.com>
2024-09-25 19:40:21 -0400

    Add files via upload

---
+++ CV.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+
+<head>
+<meta http-equiv="content-type" content="text/html;charset=utf-8"/>
+<meta http-equiv="Content-Style-Type" content="text/css"/>
+
+<title></title>
+<link href="styles/trs-style-iWeblike.css" rel="stylesheet" type="text/css" />
+<link rel="icon" href="images-static/titan.jpg" type="image/jpg" />
+</head>
+
+
+<body>
+
+<div id="navigation">
+
+<table>
+<tr>
+<td><a href="index.php">Home</a></td>
+<td><a href="research.php">Research</a></td>
+<td><a href="teaching.php">Teaching</a></td>
+<td><a href="CV.php">CV</a></td>
+<td><a href="other.php">Other</a></td>
+</tr>
+</table>
+
+
+<div class="hr"> <hr /> </div>
+
+
+
+
+</div>   <! End of navigation div -->
+
+<div id="name">
+Emmanuel Hartman CV Last Updated: 6/25/2024
+</div>
+
+<div id="namec">
+<iframe src="https://drive.google.com/file/d/1sNNzFt_Qs6s64h4Pewu-5rAt_1VjxS4q/preview" width="800" height="600"></iframe>
+
+</div>
+
+
+
+
+<div id="name">
+</div>
+<div id="name">
+</div>
+<div id="name">
+</div>
+<div id="name">
+</div>
+<div id="name">
+</div>
+<div id="name">
+</div>
+
+
+<div id="main">
+
+
+<div id="footer">
+
+<br clear="all" />
+
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
+
+
---
+++ teaching.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"><head>
+<meta http-equiv="content-type" content="text/html;charset=utf-8">
+<meta http-equiv="Content-Style-Type" content="text/css">
+
+<title></title>
+<link href="styles/trs-style-iWeblike.css" rel="stylesheet" type="text/css">
+<link rel="icon" href="images-static/titan.jpg" type="image/jpg">
+</head>
+
+
+<body data-gr-c-s-loaded="true">
+
+<div id="navigation">
+
+<table>
+<tr>
+<td><a href="index.php">Home</a></td>
+<td><a href="research.php">Research</a></td>
+<td><a href="teaching.php">Teaching</a></td>
+<td><a href="CV.php">CV</a></td>
+<td><a href="other.php">Other</a></td>
+</tr>
+</table>
+
+
+<div class="hr"> <hr> </div>
+
+
+
+
+</div>   <!-- End of navigation div ---->
+
+<div id="name">
+My Teaching Page
+</div>
+
+<div id="main">
+<b>SPRING 2023: </b>
+
+<br><br>
+ 
+-Linear Algebra I Instructor
+
+<br><br>
+
+<b>SUMMER 2022: </b>
+
+<br><br>
+ 
+-Calculus III Instructor
+
+<br><br>
+
+<b>SPRING 2022: </b>
+
+<br><br>
+ 
+-Calculus II Instructor
+
+<br><br>
+
+<b>SUMMER 2021: </b>
+
+<br><br>
+ 
+-Calculus I Instructor
+
+<br><br>
+
+
+<b>SUMMER 2020: </b>
+
+<br><br>
+
+-Discrete Math Recitation Instructor
+
+<br><br>
+
+<b>SPRING 2020: </b>
+
+<br><br>
+
+-Precalculus Instructor <br>
+
+-College Algebra Teaching Assistant
+
+<br><br>
+
+<b>FALL 2019: </b><br>
+<br>
+
+-Precalculus Algebra Instructor <br>
+
+-Precalculus Teaching Assistant
+<br><br>
+
+<b>SUMMER 2019: </b><br><br>
+
+-Buisness Calculus Teaching Assistant<br>
+
+- Trigonometry Teaching Assistant
+<br><br>
+
+<b>SPRING 2019: </b><br><br>
+
+-Linear Algebra Grader
+
+<br><br>
+
+<b>FALL 2018:</b><br><br>
+
+-Trigonometry Teaching Assistant<br>
+
+-College Algebra Teaching Assistant<br>
+<div id="footer">
+
+<br clear="all">
+
+</div>
+</div>
+
+
+</body></html>
+
+
+
